1cc964599aa8b28329cd7257abea599ad2ba0d3a
Julia <julia.maliutsina@gmail.com>
2022-04-30 01:59:26 +0300

    App created

---
+++ catalog/src/components/Menu/styled.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+export const MenuWrapper = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: flex-start;
+  padding: 29px 101px 0px 117px;
+`;
+
+export const MenuCategories = styled.div`
+  width: 234px;
+  color: #1d1f22;
+  font-family: 'Raleway';
+  font-size: 16px;
+  font-weight: 400;
+  text-transform: uppercase;
+  flex-grow: 1;
+  flex-shrink: 0;
+`;
+
+export const MenuCategory = styled.div`
+  height: 51px;
+  margin: 0px 16px;
+  display: inline-block;
+  &:hover {
+    color: #5ece7b;
+    border-bottom: 2px solid #5ece7b;
+    cursor: pointer;
+  }
+`;
+
+export const Logo = styled.img`
+  flex-grow: 1;
+  flex-shrink: 0;
+  height: 41px;
+  margin-top: -5px;
+`;
+export const Actions = styled.div`
+  flex-grow: 1;
+  flex-shrink: 0;
+  text-align: right;
+`;
+
+export const CurrencyButton = styled.div`
+  display: inline-block;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const CartButton = styled.div`
+  display: inline-block;
+  margin-left: 22px;
+  margin-top: 6px;
+
+  vertical-align: top;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const Currency = styled.img``;
+
+export const Arrow = styled.img`
+  vertical-align: text-top;
+  margin-top: 3px;
+`;
+
+export const Cart = styled.img``;
+
+export const SpaceHolder = styled.div`
+  flex-grow: 2;
+  flex-shrink: 1;
+`;
---
+++ catalog/src/images/index.js
@@ -0,0 +1,4 @@
+export { default as arrowIcon } from './arrow.svg';
+export { default as cartIcon } from './cart.svg';
+export { default as currencyIcon } from './currency.svg';
+export { default as logo } from './logo.svg';
